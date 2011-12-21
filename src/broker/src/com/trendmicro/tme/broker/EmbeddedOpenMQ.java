2c21cddfccc1ba43fd93b51aebc91f9076ac9df7
scott_wang <devnull@localhost>
2011-12-21 16:00:51 +0800

    Listen to events from zookeeper in EmbeddedOpenMQ

--- src/broker/src/com/trendmicro/tme/broker/EmbeddedOpenMQ.java
+++ src/broker/src/com/trendmicro/tme/broker/EmbeddedOpenMQ.java
@@ -4,43 +4,57 @@ import java.io.FileInputStream;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Date;
+import java.util.Map;
 import java.util.Properties;
 
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.protobuf.TextFormat;
 import com.sun.messaging.jmq.jmsclient.runtime.BrokerInstance;
 import com.sun.messaging.jmq.jmsclient.runtime.ClientRuntime;
 import com.trendmicro.codi.CODIException;
+import com.trendmicro.codi.DataListener;
+import com.trendmicro.codi.DataObserver;
 import com.trendmicro.codi.ZKSessionManager;
 import com.trendmicro.codi.ZNode;
+import com.trendmicro.codi.lock.ZLock;
+import com.trendmicro.codi.lock.Lock.LockType;
 import com.trendmicro.mist.proto.ZooKeeperInfo;
 
-public class EmbeddedOpenMQ {
+public class EmbeddedOpenMQ implements DataListener {
     private final static Logger logger = LoggerFactory.getLogger(EmbeddedOpenMQ.class);
     private BrokerInstance brokerInstance;
     private Properties config;
     private String host;
+    private boolean statusChanged = false;
+    private boolean isOnline = false;
+    private DataObserver obs;
+    private String mqHome;
+    private String mqVar;
     
     public EmbeddedOpenMQ(String mqHome, String mqVar, Properties config) throws Exception {
         host = InetAddress.getLocalHost().getHostAddress();
-        
+        this.mqHome = mqHome;
+        this.mqVar = mqVar;
+        this.config = config;
+        obs = new DataObserver("/tme2/broker/" + host, this, false, 0);
+    }
+    
+    public void start() throws Exception {
+        logger.info("Starting OpenMQ Broker...");
         ClientRuntime clientRuntime = ClientRuntime.getRuntime();
         brokerInstance = clientRuntime.createBrokerInstance();
         
-        this.config = brokerInstance.parseArgs(new String[] {
+        Properties brokerConfig = brokerInstance.parseArgs(new String[] {
             "-imqhome", mqHome, "-varhome", mqVar
         });
-        this.config.putAll(config);
-        
-        brokerInstance.init(this.config, null);
-    }
-    
-    public void start() {
-        logger.info("Starting OpenMQ Broker...");
+        brokerConfig.putAll(config);
+        brokerInstance.init(brokerConfig, null);
         brokerInstance.start();
         logger.info("OpenMQ Broker started!");
+        isOnline = true;
     }
     
     public void stop() {
@@ -48,7 +62,8 @@ public class EmbeddedOpenMQ {
         brokerInstance.stop();
         logger.info("Shutting down OpenMQ Broker...");
         brokerInstance.shutdown();
-        logger.info("Bye!");
+        logger.info("OpenMQ Broker is down.");
+        isOnline = false;
     }
     
     public void registerOnZk() throws CODIException, UnknownHostException {
@@ -71,33 +86,90 @@ public class EmbeddedOpenMQ {
         
         ZNode loadingNode = new ZNode("/tme2/broker/" + host + "/loading");
         loadingNode.create(false, ZooKeeperInfo.Loading.newBuilder().setLoading(0).setLastUpdate(0).setFreeMemory(0).setMaxMemory(0).build().toString());
+        obs.start();
     }
     
     public void unregisterOnZk() throws CODIException {
+        obs.stop();
         ZNode brkNode = new ZNode("/tme2/broker/" + host);
         brkNode.deleteRecursively();
+    }
+    
+    private ZooKeeperInfo.Broker.Status getNewStatus() {
+        try {
+            byte[] data = new ZNode("/tme2/broker/" + host).getContent();
+            ZooKeeperInfo.Broker.Builder builder = ZooKeeperInfo.Broker.newBuilder();
+            TextFormat.merge(new String(data), builder);
+            return builder.build().getStatus();
+        }
+        catch(Exception e) {
+            logger.error("getNewStatus() error", e);
+            return null;
+        }
+    }
+    
+    private synchronized boolean checkOnlineStatus() {
+        if(statusChanged) {
+            String lockPath = "/tme2/locks/brk_" + host;
+            ZLock lock = new ZLock(lockPath);
+            try {
+                logger.info("Waiting for broker lock...");
+                lock.acquire(LockType.WRITE_LOCK);
+                logger.info("Get broker lock.");
+                ZooKeeperInfo.Broker.Status newStatus = getNewStatus();
+                if((newStatus == ZooKeeperInfo.Broker.Status.ONLINE) != isOnline) {
+                    switch(newStatus) {
+                    case OFFLINE:
+                        stop();
+                        break;
+                    case ONLINE:
+                        start();
+                        break;
+                    default:
+                        break;
+                    }
+                }
+            }
+            catch(Exception e) {
+                logger.error(e.getMessage(), e);
+            }
+            finally {
+                try {
+                    lock.release();
+                    if(lock.getChildren().isEmpty())
+                        lock.delete();
+                }
+                catch(CODIException e) {
+                }
+            }
+            
+            statusChanged = false;
+        }
+        return isOnline;
     }
     
     public void reportLoadingForever() throws InterruptedException {
         ZNode loadingNode = new ZNode("/tme2/broker/" + host + "/loading");
         while(true) {
-            long free = Runtime.getRuntime().freeMemory();
-            long total = Runtime.getRuntime().totalMemory();
-            long max = Runtime.getRuntime().maxMemory();
-            
-            ZooKeeperInfo.Loading.Builder load_builder = ZooKeeperInfo.Loading.newBuilder();
-            load_builder.setLoading(Math.round(((float) (max - free) / max) * 100));
-            load_builder.setLastUpdate(new Date().getTime());
-            load_builder.setFreeMemory(free);
-            load_builder.setMaxMemory(max);
-            try {
-                loadingNode.setContent(load_builder.build().toString());
-                logger.info(String.format("Reporting loading: free memory: %d, total memory: %d max memory: %d", free, total, max));
+            if(checkOnlineStatus()) {
+                long free = Runtime.getRuntime().freeMemory();
+                long total = Runtime.getRuntime().totalMemory();
+                long max = Runtime.getRuntime().maxMemory();
+                
+                ZooKeeperInfo.Loading.Builder load_builder = ZooKeeperInfo.Loading.newBuilder();
+                load_builder.setLoading(Math.round(((float) (max - free) / max) * 100));
+                load_builder.setLastUpdate(new Date().getTime());
+                load_builder.setFreeMemory(free);
+                load_builder.setMaxMemory(max);
+                try {
+                    loadingNode.setContent(load_builder.build().toString());
+                    logger.info(String.format("Reporting loading: free memory: %d, total memory: %d max memory: %d", free, total, max));
+                }
+                catch(CODIException e) {
+                    logger.error("Cannot report loading", e);
+                }
             }
-            catch(CODIException e) {
-                logger.error("Cannot report loading", e);
-            }
-            Thread.sleep(30000);            
+            Thread.sleep(5000);
         }
     }
     
@@ -118,17 +190,25 @@ public class EmbeddedOpenMQ {
         Runtime.getRuntime().addShutdownHook(new Thread() {
             @Override
             public void run() {
+                mq.stop();
                 try {
                     mq.unregisterOnZk();
                 }
                 catch(CODIException e) {
                     logger.error("Unable to unregister broker", e);
                 }
-                mq.stop();                
             }
         });
         
         mq.registerOnZk();
         mq.reportLoadingForever();
+    }
+    
+    @Override
+    public synchronized void onDataChanged(String arg0, Map<String, byte[]> changeMap) {
+        if(changeMap.get("") == null) // broker node is removed
+            return;
+        statusChanged = true;
+        logger.info("broker status changed");
     }
 }

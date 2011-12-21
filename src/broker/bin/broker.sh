ee2d741c895c735fa85fd2cbf7ff40b6d94af378
scott_wang <devnull@localhost>
2011-12-21 17:20:00 +0800

    make broker memory configurable

--- src/broker/bin/broker.sh
+++ src/broker/bin/broker.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+MEMORY=`sed -e '/imq\.system\.max_size/!d ; s/.*=//g' /opt/trend/tme/conf/broker/config.properties`
+
 CLASSPATH="/opt/trend/tme/conf/broker:/opt/trend/tme/lib/*"
-JVM_ARGS="-server -Xmx1g -Xms1g -Dcom.sun.management.jmxremote.port=5566 -Dcom.sun.management.jmxremote.authenticate=true -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.password.file=/opt/trend/tme/conf/broker/jmxremote.password -Dcom.sun.management.jmxremote.access.file=/opt/trend/tme/conf/broker/jmxremote.access"
+JVM_ARGS="-server -Xmx$MEMORY -Xms$MEMORY -Dcom.sun.management.jmxremote.port=5566 -Dcom.sun.management.jmxremote.authenticate=true -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.password.file=/opt/trend/tme/conf/broker/jmxremote.password -Dcom.sun.management.jmxremote.access.file=/opt/trend/tme/conf/broker/jmxremote.access"
 
 if [ "$1" == "daemon" ]
 then

9990a7fb8b60baba818b0fd30838dff2f30bf1d4
Scott Wang <scott_wang@hotmail.com>
2012-01-13 14:02:39 +0800

    add detach and unmount function for mist_source

--- src/mistic/mist_source.cpp
+++ src/mistic/mist_source.cpp
@@ -66,6 +66,22 @@ void attach(const string& session_id, bool ack){
 	}
 }
 
+void detach(const string& session_id){
+	Command req_cmd;
+	Command res;
+	Request* req_ptr = req_cmd.add_request();
+	req_ptr->set_type(Request::CLIENT_DETACH);
+	req_ptr->set_argument(session_id);
+	if (sendRequest(req_cmd, res) == 0){
+	    if (res.response(0).success()){
+		cerr<<res.response(0).context()<<endl;
+	    }
+	    else{
+		cerr<<res.response(0).exception()<<endl;
+	    }
+	}
+}
+
 void mount(const string& session_id, const string& exName) {
 	Command cmd;
 	Client* clientPtr = cmd.add_client();
@@ -74,6 +90,26 @@ void mount(const string& session_id, const string& exName) {
 	clientPtr->mutable_channel()->set_type(Channel::QUEUE);
 	clientPtr->set_type(Client::CONSUMER);
 	clientPtr->set_action(Client::MOUNT);
+
+	Command res;
+	if (sendRequest(cmd, res) == 0){
+		if (res.response(0).success()){
+			cerr<<res.response(0).context()<<endl;
+		}
+		else{
+					cerr<<res.response(0).exception()<<endl;
+				}
+	}
+}
+
+void unmount(const string& session_id, const string& exName) {
+	Command cmd;
+	Client* clientPtr = cmd.add_client();
+	clientPtr->set_session_id(atoi(session_id.c_str()));
+	clientPtr->mutable_channel()->set_name(exName);
+	clientPtr->mutable_channel()->set_type(Channel::QUEUE);
+	clientPtr->set_type(Client::CONSUMER);
+	clientPtr->set_action(Client::UNMOUNT);
 
 	Command res;
 	if (sendRequest(cmd, res) == 0){
@@ -105,14 +141,13 @@ int main(int argc, char* argv[]) {
 
 	program_opt::options_description opt_desc("Allowed options");
 	opt_desc.add_options()("help", "Display help messages")("attach,a", "Attach session")("session-id,s", "Session ID")
-			("ack,A", "Manual ACK messages")("mount,m", program_opt::value<string>(),"Mount exchange");
+			("ack,A", "Manual ACK messages")("mount,m", program_opt::value<string>(),"Mount exchange")
+			("unmount,u", program_opt::value<string>(), "Unmount exchange")("detach,d", "Detach session");
 
 	program_opt::positional_options_description pos_opt_desc;
 	pos_opt_desc.add("session-id", -1);
 
 	program_opt::variables_map var_map;
-	//program_opt::store(program_opt::parse_command_line(argc, argv, opt_desc),
-		//	var_map);
 	program_opt::store(program_opt::command_line_parser(argc, argv).options(opt_desc).positional(pos_opt_desc).run(), var_map);
 	program_opt::notify(var_map);
 
@@ -127,6 +162,12 @@ int main(int argc, char* argv[]) {
 	}
 	else if (var_map.count("mount")) {
 		mount(var_map["session-id"].as<string>(), var_map["mount"].as<string>());
+	}
+	else if (var_map.count("unmount")) {
+		unmount(var_map["session-id"].as<string>(), var_map["unmount"].as<string>());
+	}
+	else if (var_map.count("detach")) {
+		detach(var_map["session-id"].as<string>());
 	}
 	else {
 		cerr << opt_desc << endl;

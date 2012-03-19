c32dc8a9419e21a49c32056af1e953ea341cd983
Scott Wang <scott_wang@hotmail.com>
2012-03-19 17:04:45 +0800

    refactor javascript

--- src/portal-web/src/public/javascript/portal.js
+++ src/portal-web/src/public/javascript/portal.js
@@ -1,36 +1,39 @@
 
+var refreshScript = 'location.reload();';
+function refresh(){
+	eval(refreshScript);
+	setTimeout('refresh();', 30000);
+}
+
 if($.cookie('range') == null){
 	$.cookie('range', '10.minutes');
 }
 
-var renew = "";
-var selected = "";
+function clearSelected(){
+	$('#filter')[0].value = '';
+	$.cookie('filter', '');
+	$.uiTableFilter($('table#exchanges'), '');
+	$('.selectedExchange').each(function(){this.checked = false});
+}
+
+function invertSelected(){
+	filter=$('#filter')[0].value;
+	$(".selectedExchange").each(function(){
+		if(filter.length == 0 || this.name.match(filter) != null){
+			this.checked = !this.checked;
+		}
+	});
+}
 
 function getSelected(){
-	selected = "";
+	var selected = "";
 	$(".selectedExchange:checked").each(function(){
 		selected = selected + this.name + ","
 	});
 	return selected;
 }
 
-function getMerge(){
-	$.ajax({
-	        type: "POST",
-	        async: false,
-	        headers: {accept: 'text/javascript'},
-	        url: "/merge",
-	        data: "selected=" + selected,
-	        success: function(res){
-	                eval(res);
-	        },
-	        error: function(xhr,text,err){
-	                alert(err);
-	        }
-	});
-}
-
-function setMergeRange(range){
+function setRange(range){
 	$.cookie('range', range);
 	$('.range-button').css('background-color', 'white');
 	$('.range-button').each(function(){
@@ -40,45 +43,77 @@ function setMergeRange(range){
 	});
 }
 
-function merge(){
-	selected = getSelected();
-	if(selected.length == 0){
-		alert('No exchange selected!');
-		return;
-	}
-	
-	$('#rrdmodal > .modal-header > h5').empty().append('<span>Merged Result</span>&nbsp;&nbsp;&nbsp')
-		.append('<input class="range-button" type=button value="10.minutes" onclick="setMergeRange(this.value); getMerge();">&nbsp;')
-		.append('<input class="range-button" type=button value="1.hour" onclick="setMergeRange(this.value); getMerge();">&nbsp;')
-		.append('<input class="range-button" type=button value="6.hour" onclick="setMergeRange(this.value); getMerge();">&nbsp;')
-		.append('<input class="range-button" type=button value="1.day" onclick="setMergeRange(this.value); getMerge();">&nbsp;')
-		.append('<input class="range-button" type=button value="1.week" onclick="setMergeRange(this.value); getMerge();">&nbsp;')
-		.append('<input class="range-button" type=button value="1.month" onclick="setMergeRange(this.value); getMerge();">&nbsp;')
+function postMerge(){
+	$.ajax({
+		type: 'POST',
+		url: '/merge',
+		data: 'selected=' + getSelected(),
+		dataType: 'script',
+		success: function(){
+			refreshScript = 'postMerge()';
+		},
+	});
+}
 
-	$("#rrdmodal > .modal-body")
+function openModal(title, initContent, updateMethod){
+	$('#rrdmodal > .modal-header > h5').empty().append('<span>' + title + '</span><br>')
+		.append('<input class="range-button" type=button value="10.minutes" onclick="setRange(this.value);' + updateMethod + '">&nbsp;')
+		.append('<input class="range-button" type=button value="1.hour" onclick="setRange(this.value);' + updateMethod + '">&nbsp;')
+		.append('<input class="range-button" type=button value="6.hour" onclick="setRange(this.value);' + updateMethod + '">&nbsp;')
+		.append('<input class="range-button" type=button value="1.day" onclick="setRange(this.value);' + updateMethod + '">&nbsp;')
+		.append('<input class="range-button" type=button value="1.week" onclick="setRange(this.value);' + updateMethod + '">&nbsp;')
+		.append('<input class="range-button" type=button value="1.month" onclick="setRange(this.value);' + updateMethod + '">&nbsp;');
+
+	$('#rrdmodal > .modal-body')
 		.empty()
-		.append("<h1>Merging...</h1>");
-	
-	$("#rrdmodal").modal({
+		.append('<h1>' + initContent + '</h1>');
+
+	$('#rrdmodal').css('max-height', window.innerHeight - 90 + 'px').css('top', '350px');
+
+	$('#rrdmodal').modal({
 		keyboard: true,
 		backdrop: true,
 		show: true,
 	});
+}
+
+function merge(){
+	if(getSelected().length == 0){
+		alert('No exchange selected!');
+		return;
+	}
 	
-	setMergeRange($.cookie('range'));
-	
-	$.ajax({
-		type: "POST",
-		headers: {accept: 'text/javascript'},
-		url: "/merge",
-		data: "selected=" + selected,
-		success: function(res){
-			eval(res);
-			renew = "merge";
-			renew_merge();
-		},
-		error: function(xhr, text, err){
-			alert(err);
-		},
+	openModal('Merged Result', 'Merging...', 'postMerge();');
+	setRange($.cookie('range'));
+	postMerge();
+}
+
+function getExchangeRequest(name){
+	$.getScript('/exchanges/' + name, function(){
+		refreshScript = "getExchangeRequest('" + name + "')";
 	});
 }
+
+function getExchange(exchange){
+	name = exchange.attr('rrd');
+	openModal(name, 'Fetching...', "getExchangeRequest('" + name + "');");
+	setRange($.cookie('range'));
+	getExchangeRequest(name);
+}
+
+$(document).ready(function() {
+	$('#rrdmodal').on('hidden', function () {
+		refreshScript = 'location.reload();';
+	});
+	setTimeout('refresh();', 30000);
+
+	$('.rrdmodal-trigger').click(function() {
+		getExchange($(this));
+	});
+
+	$('a[rel=popover]').popover({
+        offset: 10
+    }).click(function(e) {
+        e.preventDefault()
+    });
+});
